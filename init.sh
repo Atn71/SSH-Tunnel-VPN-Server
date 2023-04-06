8428d341f7428e0d62d33298bd131f243fd66318
MMDBadCoder <gitmmdgit@gmail.com>
2023-04-06 16:15:10 +0330

    add service to init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,16 @@ pip install -r requirements.txt
 rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
+
+running_file=$(readlink -f runserver.sh)
+
+echo "[Unit]
+Description=MMD VPN Startup Script
+
+[Service]
+ExecStart=$running_file
+
+[Install]
+WantedBy=multi-user.target" >mmdvpn.service
+
+mv mmdvpn.service /lib/systemd/system/mmdvpn.service
